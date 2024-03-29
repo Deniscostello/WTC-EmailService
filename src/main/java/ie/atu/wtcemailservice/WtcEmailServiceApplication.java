51bccc6373fa11ca3ce209f831c093c8bb266869
Denis Costello <G00396309@atu.ie>
2024-03-29 22:52:26 +0000

    First commit

---
+++ src/main/java/ie/atu/wtcemailservice/WtcEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.wtcemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtcEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtcEmailServiceApplication.class, args);
+    }
+
+}
